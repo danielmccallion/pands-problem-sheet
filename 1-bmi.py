7f2b1834b7d53fc3bfe195dbcc432418cfd20ef6
Daniel Mc Callion <dmceire@gmail.com>
2020-02-15 17:13:08 +0000

    Added new file for calculating bmi. Problem Sheet 1. Weekly task 2

---
+++ 1-bmi.py
@@ -0,0 +1,18 @@
+# This program  calculates somebody's Body Mass Index (BMI).
+# The inputs are the person's height in centimetres and weight in kilograms.
+# The output is their weight divided by their height in metres squared. 
+# Expected outcome:
+# Enter weight: 65
+# Enter height: 180
+# BMI is 20.06.
+
+weight = int(input("Enter weight: "))
+height = int(input("Enter height: "))
+
+height_metres = height/100
+
+height_squared = height_metres ** 2
+
+bmi = weight / height_squared
+
+print(f"BMI is {bmi:.2f}.")
