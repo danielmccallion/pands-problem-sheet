bcdee446a7501d85a22806988c73f0a771f7b0f4
Daniel Mc Callion <dmceire@gmail.com>
2020-02-16 18:38:17 +0000

    Added new file to take a string and output every second character in reverse order. Problem Sheet 2. Weekly task 3

--- 1-bmi.py
+++ 1-bmi.py
@@ -1,3 +1,4 @@
+# Daniel Mc Callion
 # This program  calculates somebody's Body Mass Index (BMI).
 # The inputs are the person's height in centimetres and weight in kilograms.
 # The output is their weight divided by their height in metres squared. 
---
+++ 2-secondstring.py
@@ -0,0 +1,9 @@
+# Daniel Mc Callion
+# This program asks a user to input a string.
+# It then outputs every second letter in reverse order.
+
+# Get users string
+users_string = input("Please enter a sentence: ")
+
+# Output every second character in reverse order.
+print(f"{users_string[::-2]}")
